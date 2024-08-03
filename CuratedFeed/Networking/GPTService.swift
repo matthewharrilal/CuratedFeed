528327fdf060bbad1e7bca67b56477a80087225a
Space Wizard <spacewizardmoneygang@Spaces-MBP.lan>
2024-08-03 12:50:12 -0400

    Creating GPT Service layer for encoding and decoding response from encoded request

--- CuratedFeed/Networking/GPTService.swift
+++ CuratedFeed/Networking/GPTService.swift
@@ -6,3 +6,81 @@
 //
 
 import Foundation
+
+protocol GPTServiceProtocol: AnyObject {
+    func executeRequest(prompt: String, maxTokens: Int) async -> GPTResponse?
+}
+
+class GPTService {
+    
+    struct Constants {
+        static let authorizationHeaderField: String = "Authorization"
+        static let contentTypeHeaderField: String = "Content-Type"
+        static let contentValue: String = "application/json"
+        static let postMethodType: String = "POST"
+        static let gptModel: String = "text-davinci-003"
+        static let temperature: Double = 0.7
+    }
+    
+    private var apiKey: String? {
+        ProcessInfo.processInfo.environment["API_KEY"]
+    }
+    
+    private var urlString: String {
+        "https://api.openai.com/v1/completions"
+    }
+}
+
+extension GPTService: GPTServiceProtocol {
+    
+    func executeRequest(prompt: String, maxTokens: Int) async -> GPTResponse? {
+        guard
+            let apiKey = apiKey,
+            let url = URL(string: urlString)
+        else { return nil }
+        
+        var request = URLRequest(url: url)
+        encodeURLRequest(
+            request: &request,
+            prompt: prompt,
+            maxTokens: maxTokens,
+            apiKey: apiKey
+        )
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+            let response = try JSONDecoder().decode(GPTResponse.self, from: data)
+            return response
+        }
+        catch {
+            print("Trouble executing and decoding results for GPT Request")
+            return nil
+        }
+    }
+    
+    func encodeURLRequest(request: inout URLRequest, prompt: String, maxTokens: Int, apiKey: String) {
+        request.addValue(
+            "Bearer \(apiKey)",
+            forHTTPHeaderField: Constants.authorizationHeaderField
+        )
+        request.addValue(
+            Constants.contentValue,
+            forHTTPHeaderField: Constants.contentTypeHeaderField
+        )
+        request.httpMethod = Constants.postMethodType
+        
+        let requestBody = GPTRequest(
+            model: Constants.gptModel,
+            prompt: prompt,
+            temperature: Constants.temperature,
+            maxTokens: maxTokens
+        )
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(requestBody)
+        }
+        catch {
+            print("Trouble encoding request body to URL Request")
+        }
+    }
+}
